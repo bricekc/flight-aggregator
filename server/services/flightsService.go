76a644cf2370e27f01124c8c623c7c73449dcaa9
CorentinCLERO <corentinclero@hotmail.com>
2025-09-23 14:38:46 +0200

    push transformed types

---
+++ server/services/flightsService.go
@@ -0,0 +1,26 @@
+package services
+
+type Flight struct {
+	Number string
+	From   string
+	To     string
+	Depart string
+	Arrive string
+}
+
+type Total struct {
+	Price    float64
+	Currency string
+}
+
+type Travelling struct {
+	BookingId     string
+	Status        string
+	PassengerName string
+	Flights       []Flight
+	Total         Total
+}
+
+type Travellings struct {
+	Travellings []Travelling
+}
