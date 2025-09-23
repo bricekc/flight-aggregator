95daf5bd60bd17734f12ee381a202cb6f999181c
CorentinCLERO <corentinclero@hotmail.com>
2025-09-23 12:13:37 +0200

    init server

--- server/main.go
+++ server/main.go
@@ -5,8 +5,26 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("starting point !")
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request here")
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString((r.URL.Path)))
+	})
+
+	server := http.Server{
+		Addr: ":3001",
+	}
+
+	log.Println("Server start on : http://localhost:3001")
+
+	err := server.ListenAndServe()
+
+	log.Fatalln(err)
 }
